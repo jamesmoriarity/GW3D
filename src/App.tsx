2082d88893715d11eca2a9a4b7c8d7a919ed5f65
jamesmoriarity <james@moriarity.org>
2022-01-08 17:55:47 -1000

    updated loading model

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,68 @@
-import { enablePatches } from '@reduxjs/toolkit/node_modules/immer';
-import React, { Component, useState } from 'react';
+import { Component } from 'react';
 import './App.css';
-import { selectState } from './features/grid/State/Selectors/GridStateSelectors';
 import { NameNotes } from './features/nameNotes/Components/NameNotes/NameNotes';
-import {gsap} from 'gsap'
 import { Vector3 } from 'three';
+import DatGui, { DatFolder, DatNumber} from 'react-dat-gui';
+import { objectTraps } from '@reduxjs/toolkit/node_modules/immer/dist/internal';
 
+export interface DatSettingsProps{
+  data:any,
+  handleUpdate:any
+}
+export class DatSettings extends Component{
+  props!:DatSettingsProps
+  constructor(props:DatSettingsProps){
+    super(props)
+  }
+  render(){
+    return  <DatGui data={this.props.data} onUpdate={this.props.handleUpdate}>
+              <DatNumber path='lightIntensity' label='Light Intensity' min={0} max={10} step={0.25} />
+              <DatNumber path='cubeCount' label='Number of Cubes' min={0} max={10} step={1} />
+              <DatFolder title='Camera Position' closed={true}>
+                <DatNumber path='cameraPos.x' label='Camera X' min={-10} max={10} step={0.5} />
+                <DatNumber path='cameraPos.y' label='Camera Y' min={0} max={40} step={0.5} />
+                <DatNumber path='cameraPos.z' label='Camera Z' min={-10} max={10} step={0.5} />
+              </DatFolder>
+              <DatFolder title='Camera Look At' closed={true}>
+                <DatNumber path='lookAtPos.x' label='X' min={-10} max={10} step={0.5} />
+                <DatNumber path='lookAtPos.y' label='Y' min={0} max={40} step={0.5} />
+                <DatNumber path='lookAtPos.z' label='Z' min={-10} max={10} step={0.5} />
+              </DatFolder>
+            </DatGui>
+  }
+}
 export class App extends Component{
-  
-  state:any = {lookAtPos:new Vector3(), cameraPos:new Vector3(0,2.75,7)}
+  state:any = {
+    lookAtPos:new Vector3(-0.5, 1, 0), 
+    cameraPos:new Vector3(0, 1, 3),
+    guitarRotation:new Vector3(-0.75,-0.5,0),
+    lightIntensity:1,
+    cubeCount:0,
+  }
   constructor(props:any){
     super(props)
   }
-  cameraPan = (direction:number) => {
-    let newCameraPos = this.state.cameraPos.clone()
-    newCameraPos.x += (1.5  * direction)
-    newCameraPos.y += (2.25  * direction)
-    newCameraPos.z += (1.25  * direction)
-    let newLookAtPos = this.state.lookAtPos.clone()
-    newLookAtPos.x += direction
-    this.setState({lookAtPos:newLookAtPos, cameraPos:newCameraPos})
+  handleUpdate = (newData:any) => {
+    this.setState(
+      {
+        lightIntensity:newData.lightIntensity, 
+        cubeCount:newData.cubeCount, 
+        cameraPos:newData.cameraPos,
+        lookAtPos:newData.lookAtPos
+      })
   }
   render(){
+    // console.log('render')
     return (
       <div className="App">
-        camera look: 
-        <span onClick={(event:any)=>{this.cameraPan(-1)}}> left</span>  -  <span onClick={(event:any)=>{this.cameraPan(1)}}>right </span>
-        <header className="App-header">
-          <NameNotes cameraY={this.state.cameraPos.y} lookAtPos={this.state.lookAtPos} cameraPos={this.state.cameraPos} />
-        </header>
+        <DatSettings data={{...this.state}} handleUpdate={this.handleUpdate}/>
+        <NameNotes 
+          cubeCount={this.state.cubeCount} 
+          lightIntensity={this.state.lightIntensity} 
+          lookAtPos={this.state.lookAtPos} 
+          cameraPos={this.state.cameraPos}
+          guitarRotation={this.state.guitarRotation}
+        />
       </div>
     );
   }
--- src/features/nameNotes/Components/NameNotes/NameNotes.jsx
+++ src/features/nameNotes/Components/NameNotes/NameNotes.jsx
@@ -4,34 +4,91 @@ import ReactDOM from "react-dom";
 import * as THREE from "three";
 import { setTimeout } from "timers";
 import {gsap} from 'gsap'
-
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import { Vector3 } from "three";
 export class NameNotes extends Component {
   scene = null
   camera = null
   cameraPos = {x:0, y:0, z:0}
   lookAtPos = null
+  lightIntensity = 2
+  lightIntensityTween = null
+  gltf = null
+  guitarRotation = null
   constructor(props){
     super(props)
     this.cameraY = props.cameraPos.y
     this.lookAtPos = props.lookAtPos
     this.cameraPos = props.cameraPos
+    this.lightIntensity = this.props.lightIntensity
+    this.cubeCount = this.props.cubeCount
+    this.guitarRotation = this.props.guitarRotation
   }
-  componentDidUpdate(){
-    if(this.props.cameraPos !== this.cameraPos || this.props.lookAtPos !== this.lookAtPos){
-      this.animateCamera(this.props.cameraPos, this.props.lookAtPos)
-    }
-  }
-  componentDidMount() {
-    console.log('componentDidMount')
-    this.loadModel()
-    
-  }
-  loadModel = () => {
-    // when model is loaded call rebuild
+
+  onModelLoaded = (gltf) => {
+    this.gltf = gltf
+    const scaleFactor = .1
+    this.gltf.scene.scale.set(scaleFactor, scaleFactor, scaleFactor)
+    this.gltf.scene.castShadow = true
+    this.gltf.scene.position.set(0,0,0)
+    this.gltf.scene.children[0].castShadow = true      
     this.rebuildAndRenderScene()
   }
 
+  loadModel = (comp) => {
+    const loader = new GLTFLoader();
+    loader.load(
+      'models/gibson/scene.gltf',
+      this.onModelLoaded,
+      function ( xhr ) {
+        console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+      },
+      function ( error ) {
+        console.log( 'An error happened' );
+      }
+    );
+  }
+
+  componentDidUpdate(){
+    if(!this.props.cameraPos.equals(this.cameraPos) || !this.props.lookAtPos.equals(this.lookAtPos)){
+      this.animateCamera(this.props.cameraPos, this.props.lookAtPos)
+    }
+    if(this.lightIntensity !== this.props.lightIntensity){
+      this.animateLightIntensity(this.props.lightIntensity)
+    }
+    this.rebuildAndRenderScene()
+  }
+  componentDidMount() {
+    console.log('componentDidMount')
+    this.loadModel(this)
+  }
+
+  animateLightIntensity = (targetIntensity) => {
+    console.log('animateLightIntensity')
+    if(this.lightIntensityTween){
+      this.lightIntensityTween.kill()
+    }
+    this.lightIntensityTween = gsap.to(
+      this, 
+      {
+        lightIntensity:targetIntensity,
+        onUpdate:()=>{
+          this.rebuildAndRenderScene()
+        }
+      }
+    )
+  }
+
   animateCamera = (targetPos, targetLookAtPos) => {
+    console.log('animateCamera')
+    /*     this.cameraPos.x = targetPos.x;
+    this.cameraPos.y = targetPos.y;
+    this.cameraPos.z = targetPos.z
+    this.lookAtPos.x = targetLookAtPos.x
+    this.lookAtPos.y = targetLookAtPos.y
+    this.lookAtPos.z = targetLookAtPos.z 
+    this.rebuildAndRenderScene()  
+    return */
     let animationObject = {nextCameraX:this.cameraPos.x, nextCameraY:this.cameraPos.y, nextCameraZ:this.cameraPos.z, nextLAx:this.lookAtPos.x, nextLAy:this.lookAtPos.y, nextLAz:this.lookAtPos.z}
     let tween2 = gsap.to(animationObject, {
       nextCameraX:targetPos.x,
@@ -40,6 +97,7 @@ export class NameNotes extends Component {
       nextLAx:targetLookAtPos.x, 
       nextLAy:targetLookAtPos.y, 
       nextLAz:targetLookAtPos.z,
+      duration:0.5,
       onUpdate:()=>{
         this.cameraPos.x = animationObject.nextCameraX;
         this.cameraPos.y = animationObject.nextCameraY;
@@ -71,44 +129,46 @@ export class NameNotes extends Component {
     plane.receiveShadow = true
     plane.position.y = -0.5
     plane.rotateX( - Math.PI / 2);
-    this.scene.add( plane );
+    // this.scene.add( plane );
   }
 
   addLighting = () => {
-    var light = new THREE.SpotLight(0xFFA500, 2, 90, 120);
-    light.position.set(-5, 10, -4);
-    light.castShadow = true;
-    this.scene.add(light);
-    var light2 = new THREE.SpotLight(0xff0000, 3, 90, 120);
+    var light2 = new THREE.SpotLight(0xffffff, this.lightIntensity, 90, 40);
     light2.position.set(-10, 20, 20);
     light2.castShadow = true;
-    this.scene.add(light2);
-    var light3 = new THREE.SpotLight(0x111199, 5, 100, 120);
+    //this.scene.add(light2);
+    var light3 = new THREE.SpotLight(0xffffff, this.lightIntensity, 100, 40);
     light3.castShadow = true;
-    light3.position.set(15, 15, 25);
-    this.scene.add(light3);
-    this.scene.add( new THREE.AmbientLight(0xff00ff) );
+    light3.position.set(15, 20, 25);
+    //this.scene.add(light3);
+    this.scene.add( new THREE.AmbientLight(0xffffff, 0.5) );
+    const lighth = new THREE.HemisphereLight( 0xffffbb, 0x080820, 1 );
+    this.scene.add( lighth );
   }
 
   addCube = (cubeX) => {
-    var geometry = new THREE.BoxGeometry( 1, 1, 1,20,20,20 );
+    var geometry = new THREE.BoxGeometry( 1, 15, 1,20,20,20 );
     var material = new THREE.MeshPhongMaterial({
       ambient: 0x333333,
       color: 0x333333,
       specular: 0xffffff,
       shininess: 10,
-      shading: THREE.SmoothShading
+      flatShading:true
     });
     var cube = new THREE.Mesh( geometry, material );
     cube.castShadow = true;
     cube.receiveShadow = true;
     cube.position.x = cubeX
+    cube.position.z = -5
     this.scene.add( cube );
   }
 
   addCubes = () => {
-    for(let i = -3; i < 3; i++){
-      this.addCube(i*1.5);
+    const cc = this.props.cubeCount
+    for(let i = 0; i < cc; i++){
+      const offsetX = (cc * 1.5)/-2
+      const cubeX = (i * 1.5) + offsetX
+      this.addCube( cubeX );
     }
   }
 
@@ -121,6 +181,12 @@ export class NameNotes extends Component {
     this.camera.updateMatrix()
   }
 
+  addModel = () => {
+    if(this.gltf){
+      this.scene.add(this.gltf.scene)
+    }
+  }
+
   buildScene = () => {
     this.scene = new THREE.Scene();
     this.scene.background = new THREE.Color( 0x999999 );
@@ -128,11 +194,12 @@ export class NameNotes extends Component {
     this.addLighting()
     this.addCubes()
     this.addPlane()
-
+    this.addModel()
   }
 
   renderScene = () => {
     var renderer = new THREE.WebGLRenderer();
+    renderer.outputEncoding = THREE.sRGBEncoding;
     renderer.shadowMap.enabled = true;
     renderer.shadowMap.type = THREE.PCFSoftShadowMap;
     renderer.setSize( window.innerWidth, window.innerHeight );
@@ -144,7 +211,6 @@ export class NameNotes extends Component {
   }
 
   render() {
-    console.log('render')
     return (
       <div ref={ref => (this.mount = ref)} />
     )
